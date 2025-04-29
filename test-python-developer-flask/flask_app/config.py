015cda07e82620a83edc4b4c8842373b2437cc04
pro100kir2 <kirilllupanov15@gmail.com>
2025-04-29 17:35:35 +0300

    .

--- test-python-developer-flask/flask_app/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY", "supersecretkey")
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:RNYBagpqfzLJgpfAqoXkofxqgBTqNvAd@shortline.proxy.rlwy.net:32828/railway"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SESSION_TYPE = 'filesystem'
-    WTF_CSRF_ENABLED = True
